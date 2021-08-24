c0626280ccc44f363438a229cf6aad40edc479bb
Salem <nlsabm@yahoo.com>
2021-08-24 19:51:55 -0400

    header tag

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Header from './components/Header';
+import './styling/css/main.css';
 function App() {
   return (
     <div>
-      App Placeholder
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from '../img/logo.png';
+import { Link } from 'react-router-dom';
+import '@fontsource/montserrat';
+const Header = () => {
+  return (
+    <>
+      <header>
+        <div className='logo-container'>
+          <Link to='/'>
+            <img src={logo} alt='reddit logo' className='logo' />
+          </Link>
+        </div>
+        <nav id='nav-bar'>
+          <ul style={{ fontFamily: 'montserrat' }} className='nav-links'>
+            <li>
+              <a href='#about' className='about'>
+                About
+              </a>
+            </li>
+            <li>
+              <a href='#how-it-works' className='how-it-works'>
+                How it Works
+              </a>
+            </li>
+            <li>
+              <Link to='/search'>Search</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default Header;
