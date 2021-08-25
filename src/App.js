6095fb7db56fc5018096e2c61dbda5eea587a197
Salem <nlsabm@yahoo.com>
2021-08-25 00:02:08 -0400

    fixing eslint errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import './styling/css/main.css';
+
 function App() {
   return (
     <div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,36 +1,35 @@
 import React from 'react';
-import logo from '../img/logo.png';
 import { Link } from 'react-router-dom';
+import logo from '../img/logo.png';
 import '@fontsource/montserrat';
-const Header = () => {
-  return (
-    <>
-      <header>
-        <div className='logo-container'>
-          <Link to='/'>
-            <img src={logo} alt='reddit logo' className='logo' />
-          </Link>
-        </div>
-        <nav id='nav-bar'>
-          <ul style={{ fontFamily: 'montserrat' }} className='nav-links'>
-            <li>
-              <a href='#about' className='about'>
-                About
-              </a>
-            </li>
-            <li>
-              <a href='#how-it-works' className='how-it-works'>
-                How it Works
-              </a>
-            </li>
-            <li>
-              <Link to='/search'>Search</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
-  );
-};
+
+const Header = () => (
+  <>
+    <header>
+      <div className="logo-container">
+        <Link to="/">
+          <img src={logo} alt="reddit logo" className="logo" />
+        </Link>
+      </div>
+      <nav id="nav-bar">
+        <ul style={{ fontFamily: 'montserrat' }} className="nav-links">
+          <li>
+            <a href="#about" className="about">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#how-it-works" className="how-it-works">
+              How it Works
+            </a>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  </>
+);
 
 export default Header;
