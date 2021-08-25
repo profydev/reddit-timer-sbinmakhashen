db4cf4f2310e446a9ab11338fe4306d1e02b728f
Salem <nlsabm@yahoo.com>
2021-08-25 18:15:45 -0400

    updated project name

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
 import Header from './components/Header';
 import './layouts/css/main.css';
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
-      </div>
+      <Normalize />
+      <Header />
     </Router>
   );
 }
