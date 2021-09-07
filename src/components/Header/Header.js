87c3885824f740c82379d0d36916cd38b66feb60
Salem <nlsabm@yahoo.com>
2021-09-06 22:25:09 -0400

    running via yarn

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,24 +6,26 @@ import Logo from './Logo';
 const Header = () => (
   <>
     <header>
-      <Logo />
-      <nav id="nav-bar">
-        <ul style={{ fontFamily: 'montserrat' }} className="nav-links">
-          <li>
-            <a href="#about" className="about">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#how-it-works" className="how-it-works">
-              How it Works
-            </a>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className="nav-container">
+        <Logo />
+        <nav id="nav-bar">
+          <ul style={{ fontFamily: 'montserrat' }} className="nav-links">
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+            <li>
+              <a href="#how-it-works" className="how-it-works">
+                How it Works
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="about">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   </>
 );
