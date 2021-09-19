c7f3f2fa74610ba5f58cfa43e9b7771507cb5af6
Salem <nlsabm@yahoo.com>
2021-09-18 23:09:37 -0400

    logo testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,26 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { MemoryRouter } from 'react-router';
 import App from './App';
 
-test('renders App', () => {
-  render(<App />);
+function setup() {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+}
+describe('Header', () => {
+  test('logo should point to the home page when clicked', () => {
+    setup();
+    const logoLink = screen.getByRole('link', { name: /logo/i });
+    userEvent.click(logoLink);
+    /*
+    **Personal note: continue the header testing when there is
+    content to test for in the home page later
+    */
+    // expect()
+  });
 });
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,6 @@ import Logo from './Logo';
 const Header = () => (
   <>
     <header>
-<<<<<<< HEAD
       <div className="nav-container">
         <Logo />
         <nav id="nav-bar">
@@ -27,26 +26,6 @@ const Header = () => (
           </ul>
         </nav>
       </div>
-=======
-      <Logo />
-      <nav id="nav-bar">
-        <ul style={{ fontFamily: 'montserrat' }} className="nav-links">
-          <li>
-            <a href="#about" className="about">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="#how-it-works" className="how-it-works">
-              How it Works
-            </a>
-          </li>
-          <li>
-            <Link to="/search">Search</Link>
-          </li>
-        </ul>
-      </nav>
->>>>>>> main
     </header>
   </>
 );
