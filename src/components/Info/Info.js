a5b19d51b475b31cc310a9df7835327972d95d20
Salem <nlsabm@yahoo.com>
2021-09-22 22:07:32 -0400

    updated about links

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -7,7 +7,10 @@ const Info = () => (
       <div className="subtitle">
         <ul>
           <li>We find the 500 top posts from the past year for a subreddit.</li>
-          <li>The data is visualized in a heatmap grouped by weekday and hour of the day.</li>
+          <li>
+            The data is visualized in a heatmap grouped by weekday and hour of
+            the day.
+          </li>
           <li>See immediately when to submit your reddit post.</li>
         </ul>
       </div>
@@ -17,11 +20,15 @@ const Info = () => (
       <div className="subtitle">
         <p>
           This app was created during a course on
-          <a href="https://profy.dev/" target="_blank" rel="noreferrer"> profy.dev </a>
-          with the goal to implement a pixel-perfect
-          real-world application with professional workflows and
-          tools like Kanban, Asana, Zeplin, GitHub, pull requests and code reviews.
-          <a href="https://profy.dev/employers" target="_blank" rel="noreferrer"> Click here for more information.</a>
+          <a href="profy.dev" target="_blank" rel="noreferrer">
+            profy.dev
+          </a>
+          with the goal to implement a pixel-perfect real-world application with
+          professional workflows and tools like Kanban, Asana, Zeplin, GitHub,
+          pull requests and code reviews.
+          <a href="profy.dev/employers" target="_blank" rel="noreferrer">
+            Click here for more information.
+          </a>
         </p>
       </div>
     </section>
