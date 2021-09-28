02f7b5c322f550d8486c2b6a562ef4a447e3b2d2
Salem <nlsabm@yahoo.com>
2021-09-27 22:17:13 -0400

    logo testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,26 +1,26 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import { MemoryRouter } from 'react-router';
-// import App from './App';
+import { MemoryRouter } from 'react-router';
+import App from './App';
 
-// function setup() {
-//   render(
-//     <MemoryRouter>
-//       <App />
-//     </MemoryRouter>,
-//   );
-// }
-// describe('Header', () => {
-//   test('logo should point to the home page when clicked', () => {
-//     setup();
-//     const logoLink = screen.getByRole('link', { name: /logo/i });
-//     userEvent.click(logoLink);
-//     /*
-//     **Personal note: continue the header testing when there is
-//     content to test for in the home page later
-//     */
-//     // expect()
-//   });
-// });
+function setup() {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
+}
+describe('Header', () => {
+  test('logo should point to the home page when clicked', () => {
+    setup();
+    const logoLink = screen.getByRole('link', { name: /logo/i });
+    userEvent.click(logoLink);
+    /*
+    **Personal note: continue the header testing when there is
+    content to test for in the home page later
+    */
+    // expect()
+  });
+});
