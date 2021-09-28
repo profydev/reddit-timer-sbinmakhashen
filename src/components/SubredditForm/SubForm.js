bd1ca17d0ae0e5c870c2ca669c361d43c4a11ec3
Salem <nlsabm@yahoo.com>
2021-09-27 21:51:18 -0400

    update link on submit

--- src/components/SubredditForm/SubForm.js
+++ src/components/SubredditForm/SubForm.js
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
 import SubContext from '../../Context/SubContext';
 
 const SubForm = () => {
-  const { subreddit, setSubreddit } = useContext(SubContext);
+  const { subreddit, setSubReddit } = useContext(SubContext);
+  const history = useHistory();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    history.push(`/search/${subreddit}`);
+  }
   return (
-    <form id="form">
+    <form id="form" onSubmit={handleSubmit}>
       <div className="form-header">
         <h1 className="title">Find the best time for a subreddit</h1>
         {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
@@ -14,7 +21,7 @@ const SubForm = () => {
         <input
           type="text"
           value={subreddit}
-          onChange={(event) => setSubreddit(event.target.value)}
+          onChange={(event) => setSubReddit(event.target.value)}
           id="sub-input"
         />
         <button type="submit" className="btn form-btn">
