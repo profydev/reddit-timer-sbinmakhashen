4c5a6613ab4e66092b524ef9fb1b194a2ae2b653
Salem <48455909+sbinmakhashen@users.noreply.github.com>
2021-10-19 21:42:32 -0400

    Mobile responsive (#11)
    * hero heatmap
    
    * hero styling
    
    * routing to search page
    
    * info sections setup
    
    * info positioning
    
    * updated about links
    
    * info link bug fix
    
    * about links bug fix
    
    * coloring info links
    
    * pixel-perfect layout & context subreddit
    
    * initial data commit
    
    * show loading spinner
    
    * responsive home page
    
    * responisve layout except footer
    
    * responsive heatmap
    
    * responsive posts table
    
    * footer overlap bug fix
    
    * readme file
    
    * initital commit
    
    * mobile navbar
    
    * responsive hamburger menu
    
    * navbar reset onclick
    
    * close navabr function
    
    * error msg layout bug fix

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import SubContext from '../../Context/SubContext';
 import '@fontsource/montserrat';
@@ -6,6 +6,37 @@ import Logo from './Logo';
 
 const Header = () => {
   const { subreddit } = useContext(SubContext);
+  const labelBurger = useRef(null);
+  const { current } = labelBurger;
+  const logo = document.querySelector('.logo');
+
+  function closeNavUl(lists, e) {
+    // close when one of the lists clicked
+    lists.forEach((li) => {
+      li.addEventListener('click', () => {
+        e.target.checked = false;
+        current.innerHTML = '&#9776;';
+      });
+    });
+
+    // close when the logo clicked
+    logo.addEventListener('click', () => {
+      e.target.checked = false;
+      current.innerHTML = '&#9776;';
+    });
+  }
+
+  // console.log(document.querySelector('.logo'));
+  function showMobileMenu(e) {
+    const allLis = document.querySelectorAll('.nav-links li');
+    if (e.target.checked) {
+      current.innerHTML = 'X';
+    } else {
+      current.innerHTML = '&#9776;';
+    }
+
+    closeNavUl(allLis, e);
+  }
 
   return (
     <>
@@ -13,6 +44,21 @@ const Header = () => {
         <div className="nav-container">
           <Logo />
           <nav id="nav-bar">
+            {/* mobile hamburger menu */}
+            <input
+              type="checkbox"
+              id="mobile-nav-check"
+              onClick={(e) => showMobileMenu(e)}
+            />
+            {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+            <label
+              htmlFor="mobile-nav-check"
+              className="burger"
+              ref={labelBurger}
+            >
+              &#9776;
+            </label>
+
             <ul style={{ fontFamily: 'montserrat' }} className="nav-links">
               <li>
                 <Link
